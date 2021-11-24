51b703c724e54d312a8d44e25e9beba4b3bf6b09
Ary de Souza Lopes <ary.s.lopes22@gmail.com>
2021-11-24 08:06:16 -0300

    adicionando dados do template

--- src/main/sql/s20212/provas/p1/ArySouzaLopes.sql
+++ src/main/sql/s20212/provas/p1/ArySouzaLopes.sql
@@ -55,29 +55,68 @@ foreign key (entrevista) references entrevista(numero),
 foreign key (pesquisa,pergunta,resposta) references resposta(pesquisa,pergunta,numero)
 );
 
+insert into cidade values (1,'Rio de Janeiro');
+insert into cidade values (2,'Niterói');
+insert into cidade values (3,'São Paulo');
 
-insert into venda values(202001,1,1,10,100.0);
-insert into venda values(202001,1,2,10,200.0);
-insert into venda values(202001,1,3,10,300.0);
-insert into venda values(202002,1,1,10,200.0);
-insert into venda values(202002,1,2,10,300.0);
-insert into venda values(202002,1,3,10,500.0);
-insert into venda values(202003,1,1,10,900.0);
-insert into venda values(202003,1,2,10,200.0);
-insert into venda values(202003,1,3,10,500.0);
-insert into venda values(202004,1,1,10,200.0);
-insert into venda values(202004,1,2,10,150.0);
-insert into venda values(202004,1,3,10,500.0);
-insert into venda values(202005,1,1,10,500.0);
-insert into venda values(202005,1,2,10,300.0);
-insert into venda values(202005,1,3,10,700.0);
-insert into venda values(202006,1,1,10,200.0);
-insert into venda values(202006,1,2,10,200.0);
-insert into venda values(202006,1,3,10,200.0);
+insert into bairro values (1,'Tijuca',1);
+insert into bairro values (2,'Centro',1);
+insert into bairro values (3,'Lagoa',1);
+insert into bairro values (4,'Icaraí',2);
+insert into bairro values (5,'São Domingos',2);
+insert into bairro values (6,'Santa Rosa',2);
+insert into bairro values (7,'Moema',3);
+insert into bairro values (8,'Jardim Paulista',3);
+insert into bairro values (9,'Higienópolis',3);
 
+insert into pesquisa values (1,'Pesquisa 1');
 
+insert into pergunta values (1,1,'Pergunta 1');
+insert into pergunta values (1,2,'Pergunta 2');
+insert into pergunta values (1,3,'Pergunta 3');
+insert into pergunta values (1,4,'Pergunta 4');
 
------------------------------------------
---
--- Acrescente seu código a partir daqui
------------------------------------------
+insert into resposta values (1,1,1,'Resposta 1 da pergunta 1');
+insert into resposta values (1,1,2,'Resposta 2 da pergunta 1');
+insert into resposta values (1,1,3,'Resposta 3 da pergunta 1');
+insert into resposta values (1,1,4,'Resposta 4 da pergunta 1');
+insert into resposta values (1,1,5,'Resposta 5 da pergunta 1');
+
+insert into resposta values (1,2,1,'Resposta 1 da pergunta 2');
+insert into resposta values (1,2,2,'Resposta 2 da pergunta 2');
+insert into resposta values (1,2,3,'Resposta 3 da pergunta 2');
+insert into resposta values (1,2,4,'Resposta 4 da pergunta 2');
+insert into resposta values (1,2,5,'Resposta 5 da pergunta 2');
+insert into resposta values (1,2,6,'Resposta 5 da pergunta 2');
+
+insert into resposta values (1,3,1,'Resposta 1 da pergunta 3');
+insert into resposta values (1,3,2,'Resposta 2 da pergunta 3');
+insert into resposta values (1,3,3,'Resposta 3 da pergunta 3');
+
+insert into resposta values (1,4,1,'Resposta 1 da pergunta 4');
+insert into resposta values (1,4,2,'Resposta 2 da pergunta 4');
+
+insert into entrevista values (1,'2020-03-01'::timestamp,1);
+insert into escolha values (1,1,1,2);
+insert into escolha values (1,1,2,2);
+insert into escolha values (1,1,3,1);
+
+insert into entrevista values (2,'2020-03-01'::timestamp,1);
+insert into escolha values (2,1,1,3);
+insert into escolha values (2,1,2,1);
+insert into escolha values (2,1,3,2);
+
+insert into entrevista values (3,'2020-03-01'::timestamp,1);
+insert into escolha values (3,1,1,4);
+insert into escolha values (3,1,2,1);
+insert into escolha values (3,1,3,1);
+
+insert into entrevista values (4,'2020-03-01'::timestamp,1);
+insert into escolha values (4,1,1,2);
+insert into escolha values (4,1,2,1);
+insert into escolha values (4,1,3,1);
+
+insert into entrevista values (5,'2020-03-01'::timestamp,1);
+insert into escolha values (5,1,1,2);
+insert into escolha values (5,1,2,1);
+insert into escolha values (5,1,3,1);
