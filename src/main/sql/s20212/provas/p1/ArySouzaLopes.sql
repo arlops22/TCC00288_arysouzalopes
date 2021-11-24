c0f2b7e3bf06547d9ffd85bcb34a4e16a403eca5
Ary de Souza Lopes <ary.s.lopes22@gmail.com>
2021-11-24 07:54:42 -0300

    criando tabelas baseado no modelo ER da P1

--- src/main/sql/s20212/provas/p1/ArySouzaLopes.sql
+++ src/main/sql/s20212/provas/p1/ArySouzaLopes.sql
@@ -3,12 +3,56 @@ DO $$ BEGIN
     PERFORM drop_tables();
 END $$;
 
-create table venda(
-    ano_mes int not null,
-    unidade int,
-    vendedor int,
-    produto int,
-    valor float
+create table cidade(
+numero int not null primary key,
+nome varchar not null
+);
+
+create table bairro(
+numero int not null primary key,
+nome varchar not null,
+cidade int not null,
+foreign key (cidade) references cidade(numero)
+);
+
+create table pesquisa(
+numero int not null,
+descricao varchar not null,
+primary key (numero)
+);
+
+create table pergunta(
+pesquisa int not null,
+numero int not null,
+descricao varchar not null,
+primary key (pesquisa,numero),
+foreign key (pesquisa) references pesquisa(numero)
+);
+
+create table resposta(
+pesquisa int not null,
+pergunta int not null,
+numero int not null,
+descricao varchar not null,
+primary key (pesquisa,pergunta,numero),
+foreign key (pesquisa,pergunta) references pergunta(pesquisa,numero)
+);
+
+create table entrevista(
+numero int not null primary key,
+data_hora timestamp not null,
+bairro int not null,
+foreign key (bairro) references bairro(numero)
+);
+
+create table escolha(
+entrevista int not null,
+pesquisa int not null,
+pergunta int not null,
+resposta int not null,
+primary key (entrevista,pesquisa,pergunta),
+foreign key (entrevista) references entrevista(numero),
+foreign key (pesquisa,pergunta,resposta) references resposta(pesquisa,pergunta,numero)
 );
 
 
